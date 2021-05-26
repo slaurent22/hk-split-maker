2716763c96311fa0f045bc8ef326d922bfbd3c92
Tobias Kunze <r@rixx.de>
2021-05-26 21:56:21 +0200

    Add big Hollow Knight style logo
    I built this header logo on ~~rock and roll~~ GIMP and curses, and most
    notably of course slaurent's project logo.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { saveAs } from "file-saver";
 import type { Config } from "../lib/lss";
 import { createSplitsXml } from "../lib/lss";
-import logo from "../asset/image/favicon.png";
+import logo from "../asset/image/logo.png";
 import ArrowButton from "./ArrowButton";
 import SplitConfigEditor from "./SplitConfigEditor";
 import SplitOutputEditor from "./SplitOutputEditor";
@@ -50,10 +50,7 @@ export default class App extends Component<AppProps, AppState> {
                             id="logo"
                             src={logo}
                             alt="HK Split Maker logo"
-                            width={45}
-                            height={42}
                         ></img>
-                        {" Hollow Knight Split Maker"}
                     </h1>
                 </header>
                 <h2>Instructions</h2>
