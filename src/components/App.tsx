deb4d9164b6954d19cc06f6512389a80c485b9d6
slaurent <slaurent22@pm.me>
2021-05-25 23:00:50 -0400

    add instructions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -43,8 +43,37 @@ export default class App extends Component<AppProps, AppState> {
         return (
             <div className="App">
                 <h1>Hollow Knight Split Maker</h1>
+                <h2>Instructions</h2>
+                <ol>
+                    <li>
+                        Find the splits you want to use from the <a
+                            href="https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/splits.txt"
+                            target="_blank"
+                        >splits.txt</a> file. For example, if you want to split
+                        on "Mask Fragment 4 (Upgrade)", then use "Mask1" as the
+                        split name.
+                    </li>
+                    <li>
+                        List your desired splits in the "splitIds" section of
+                        the configuration.
+                    </li>
+                    <li>
+                        Change the other configuration values as you see fit.
+                        But don't worry; these are easily changeable from
+                        LiveSplit later if needed!
+                    </li>
+                    <li>
+                        Click "Submit". The button will temporarily disable
+                        while in progress.
+                    </li>
+                    <li>
+                        Click "Download", and save the file to your computer.
+                        Open this file in LiveSplit via right click ➡ Open
+                        Splits ➡ From File
+                    </li>
+                </ol>
+                <h2>Input config JSON</h2>
                 <form onSubmit={this.onSubmit.bind(this)}>
-                    <h2>Input config JSON</h2>
                     <SplitConfigEditor
                         defaultValue={defaultValue}
                         onChange={this.onConfigInputChange.bind(this)}
