aefc25a11f8a1e21a19501c62d6d8848458ef87d
slaurent <slaurent22@pm.me>
2021-09-14 14:01:29 -0400

    remove Essence from boss split names

--- src/lib/splits.ts
+++ src/lib/splits.ts
@@ -103,9 +103,6 @@ function getName(description: string) {
         case "Grub": {
             return name.substr("Rescued ".length);
         }
-        case "Essence": {
-            return `${name} Essence`;
-        }
         default:
             return name;
     }
