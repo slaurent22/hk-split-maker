ab96afc42f9b7acd600cd061013711533bcf5120
thundar001 <thundar001@gmail.com>
2021-09-26 12:22:05 -0700

    Error and Warning Updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -49,12 +49,13 @@ export default class App extends Component<AppProps, AppState> {
         this.setState(newState);
     }
     public render(): ReactNode {
-        const { alertBannerVisible, } = this.state
+        const { alertBannerVisible, } = this.state;
         return (
             <div id="app">
                 <div className="alert-banner" style={alertBannerVisible ? {} : { display: "none", }}>
                     <span className="close-ab" onClick={this.handleClickCloseAlertBanner.bind(this)}>&times;</span>
-                    Intestested in contributing or suggesting ideas and splits? Check out the <a href="https://github.com/slaurent22/hk-split-maker" target="_blank">Github Project Site!</a>
+                    Interested in contributing or suggesting ideas and splits? Check out 
+                    the <a href="https://github.com/slaurent22/hk-split-maker" target="_blank" rel="noopener noreferrer">GitHub Project Site!</a>
                 </div>
                 <Header />
                 <Instructions />
