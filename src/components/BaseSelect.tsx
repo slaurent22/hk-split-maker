12989abbcad0fac64cd3d26205a95393fb264c30
slaurent <slaurent22@pm.me>
2021-10-05 14:37:08 -0400

    refactor: create BaseSelect

---
+++ src/components/BaseSelect.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import type { ReactElement } from "react";
+import React from "react";
+import type { GroupBase, Props, StylesConfig, Theme } from "react-select";
+import Select from "react-select";
+
+function createOnMenuOpen(classNamePrefix: string) {
+    // https://stackoverflow.com/a/66710895
+    // workaround for scrolling to selected item
+    return () => {
+        setTimeout(() => {
+            const selectedEl = document.getElementsByClassName(`${classNamePrefix}__option--is-selected`)[0];
+            if (selectedEl) {
+                selectedEl.scrollIntoView({ behavior: "smooth", block: "center", });
+            }
+        }, 15);
+    };
+}
+
+
+function createCustomStyles<
+    Option,
+    IsMulti extends boolean = false,
+    Group extends GroupBase<Option> = GroupBase<Option>
+>(): StylesConfig<Option, IsMulti, Group> {
+    return {
+        groupHeading: (provided) => ({
+            ...provided,
+            fontSize: "14px",
+            fontFamily: "serif",
+            fontWeight: "bold",
+            fontStyle: "italic",
+            color: "white",
+        }),
+        control: (provided) => {
+            return {
+                ...provided,
+                display: "flex",
+                border: "1px solid white",
+                borderRadius: "3px",
+                fontSize: "18px",
+                fontFamily: "serif",
+                margin: "8px 8px 8px 0",
+                backgroundColor: "#2f3136",
+            };
+        },
+        container: (provided) => {
+            return {
+                ...provided,
+                flex: "1 1 0%",
+            };
+        },
+    };
+}
+
+function customTheme(theme: Theme): Theme {
+    return {
+        ...theme,
+        spacing: {
+            ...theme.spacing,
+        },
+        colors: {
+            ...theme.colors,
+            primary: theme.colors.primary25,
+            primary75: theme.colors.primary50,
+            primary50: theme.colors.primary75,
+            primary25: theme.colors.primary,
+            danger: theme.colors.danger,
+            dangerLight: theme.colors.dangerLight,
+            neutral0: theme.colors.neutral80,
+            neutral5: theme.colors.neutral70,
+            neutral10: theme.colors.neutral60,
+            neutral20: theme.colors.neutral50,
+            neutral30: theme.colors.neutral40,
+            neutral40: theme.colors.neutral30,
+            neutral50: theme.colors.neutral20,
+            neutral60: theme.colors.neutral10,
+            neutral70: theme.colors.neutral5,
+            neutral80: theme.colors.neutral0,
+            neutral90: theme.colors.neutral0,
+        },
+    };
+}
+
+
+export default function BaseSelect<
+    Option,
+    IsMulti extends boolean = false,
+    Group extends GroupBase<Option> = GroupBase<Option>
+>(props: Props<Option, IsMulti, Group>): ReactElement {
+    return (
+        <Select {...props}
+            styles={createCustomStyles<Option, IsMulti, Group>()}
+            onMenuOpen={createOnMenuOpen(props.classNamePrefix ?? "BaseSelect")}
+            theme={customTheme}
+        />
+    );
+}
--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import type { StylesConfig } from "react-select";
 import Select from "react-select";
 import type { CategoryDefinition } from "../asset/categories/category-directory.json";
+import BaseSelect from "./BaseSelect";
 
 interface Props {
     id: string;
@@ -11,47 +11,8 @@ interface Props {
 }
 
 interface CategoryOption {
- value: string; label: string;
+    value: string; label: string;
 }
-
-const customStyles: StylesConfig<CategoryOption> = {
-    groupHeading: (provided) => ({
-        ...provided,
-        fontSize: "14px",
-        fontFamily: "serif",
-        fontWeight: "bold",
-        fontStyle: "italic",
-        color: "white",
-    }),
-    control: (provided) => {
-        return {
-            ...provided,
-            display: "flex",
-            border: "1px solid white",
-            borderRadius: "3px",
-            fontSize: "18px",
-            fontFamily: "serif",
-            margin: "8px 8px 8px 0",
-            backgroundColor: "#2f3136",
-        };
-    },
-    container: (provided) => {
-        return {
-            ...provided,
-            flex: "1 1 0%",
-        };
-    },
-};
-
-// https://stackoverflow.com/a/66710895
-const onMenuOpen = () => {
-    setTimeout(()=>{
-        const selectedEl = document.getElementsByClassName("MyDropdown__option--is-selected")[0];
-        if (selectedEl) {
-            selectedEl.scrollIntoView({ behavior: "smooth", block: "center", });
-        }
-    }, 15);
-};
 
 function defToOption({ fileName, displayName, }: CategoryDefinition): CategoryOption {
     return {
@@ -82,42 +43,14 @@ const CategorySelect: React.FC<Props> = ({
         };
     });
     return (
-        <Select<CategoryOption>
+        <BaseSelect<CategoryOption>
             id={id}
             options={optGroups}
-            styles={customStyles}
             onChange={newValue => onChange(newValue ? optionToDef(newValue) : null)}
-            onMenuOpen={onMenuOpen}
-            className ={"MyDropdown"}
-            classNamePrefix={"MyDropdown"}
+            className={"CategorySelect"}
+            classNamePrefix={"CategorySelect"}
             placeholder="Category: Select or type to search..."
             defaultValue={defaultValue ? defToOption(defaultValue) : undefined}
-            theme={theme => ({
-                ...theme,
-                spacing: {
-                    ...theme.spacing,
-                },
-                colors: {
-                    ...theme.colors,
-                    primary: theme.colors.primary25,
-                    primary75: theme.colors.primary50,
-                    primary50: theme.colors.primary75,
-                    primary25: theme.colors.primary,
-                    danger: theme.colors.danger,
-                    dangerLight: theme.colors.dangerLight,
-                    neutral0: theme.colors.neutral80,
-                    neutral5: theme.colors.neutral70,
-                    neutral10: theme.colors.neutral60,
-                    neutral20: theme.colors.neutral50,
-                    neutral30: theme.colors.neutral40,
-                    neutral40: theme.colors.neutral30,
-                    neutral50: theme.colors.neutral20,
-                    neutral60: theme.colors.neutral10,
-                    neutral70: theme.colors.neutral5,
-                    neutral80: theme.colors.neutral0,
-                    neutral90: theme.colors.neutral0,
-                },
-            })}
         />
     );
 };
