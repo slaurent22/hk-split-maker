a1072b4d9616989e9d3114fc0f6447f20e187d29
slaurent <slaurent22@pm.me>
2021-10-05 14:36:23 -0400

    add group to parseSplits

--- src/lib/splits.ts
+++ src/lib/splits.ts
@@ -4,16 +4,17 @@ import Icons from "../asset/icons/icons";
 // const SPLITS_DEFINITIONS_FILE = "./asset/splits.txt";
 const SPLITS_DEFINITIONS_REGEXP =
     /\[Description\("(?<description>.+)"\), ToolTip\("(?<tooltip>.+)"\)\]\s+(?<id>\w+),/g;
-const DESCRIPTION_NAME_REGEXP = /(?<name>.+)\s+\((?<qualifier>.+)\)/;
+export const DESCRIPTION_NAME_REGEXP = /(?<name>.+)\s+\((?<qualifier>.+)\)/;
 
-interface SplitDefinition {
+export interface SplitDefinition {
     description: string;
     tooltip: string;
     id: string;
     name: string;
+    group: string;
 }
 
-function getName(description: string): string {
+function getNameAndGroup(description: string): [string, string] {
     const match = DESCRIPTION_NAME_REGEXP.exec(description);
     if (!match) {
         throw new Error(`Invalid Description: ${description}`);
@@ -26,11 +27,11 @@ function getName(description: string): string {
 
     switch (qualifier) {
         case "Charm Notch":
-            return `${name} Notch`;
+            return [`${name} Notch`, qualifier];
         case "Stag Station":
-            return `${name} Stag`;
+            return [`${name} Stag`, qualifier];
         case "Grub": {
-            return name.substr("Rescued ".length);
+            return [name.substr("Rescued ".length), qualifier];
         }
         case "Transition":
             switch (name) {
@@ -48,16 +49,16 @@ function getName(description: string): string {
                 case "Pantheon 1-4":
                 case "Pantheon 5":
                 case "Waterways Manhole": {
-                    return `Enter ${name}`;
+                    return [`Enter ${name}`, qualifier];
                 }
                 case "Queen's Garden - QGA/Mound Entry": {
-                    return "Enter Queen's Gardens";
+                    return ["Enter Queen's Gardens", qualifier];
                 }
                 default: break;
             }
             break;
         case "Essence": {
-            return `${name} Essence`;
+            return [`${name} Essence`, qualifier];
         }
         default: {
             break;
@@ -66,79 +67,79 @@ function getName(description: string): string {
 
     switch (name) {
         case "Dream Nail - Awoken": {
-            return "Awoken Dream Nail";
+            return ["Awoken Dream Nail", qualifier];
         }
         case "Crystal Guardian 1": {
-            return "Crystal Guardian";
+            return ["Crystal Guardian", qualifier];
         }
         case "Crystal Guardian 2": {
-            return "Enraged Guardian";
+            return ["Enraged Guardian", qualifier];
         }
         case "Chains Broken - Hollow Knight": {
-            return "Hollow Knight Scream";
+            return ["Hollow Knight Scream", qualifier];
         }
         case "Radiance Dream Entry": {
-            return "Hollow Knight";
+            return ["Hollow Knight", qualifier];
         }
         case "Colosseum Fight 1": {
-            return "Trial of the Warrior";
+            return ["Trial of the Warrior", qualifier];
         }
         case "Colosseum Fight 2": {
-            return "Trial of the Conqueror";
+            return ["Trial of the Conqueror", qualifier];
         }
         case "Colosseum Fight 3": {
-            return "Trial of the Fool";
+            return ["Trial of the Fool", qualifier];
         }
         case "Pantheon 1": {
-            return "Pantheon of the Master";
+            return ["Pantheon of the Master", qualifier];
         }
         case "Pantheon 2": {
-            return "Pantheon of the Artist";
+            return ["Pantheon of the Artist", qualifier];
         }
         case "Pantheon 3": {
-            return "Pantheon of the Sage";
+            return ["Pantheon of the Sage", qualifier];
         }
         case "Pantheon 4": {
-            return "Pantheon of the Knight";
+            return ["Pantheon of the Knight", qualifier];
         }
         case "Pantheon 5": {
-            return "Pantheon of Hallownest";
+            return ["Pantheon of Hallownest", qualifier];
         }
         case "Aspid Hunter": {
-            return "Aspid Arena";
+            return ["Aspid Arena", qualifier];
         }
         case "Husk Miner": {
-            return "Myla";
+            return ["Myla", qualifier];
         }
         case "Kingsoul Fragment - Queen's": {
-            return "Queen Fragment";
+            return ["Queen Fragment", qualifier];
         }
         case "Kingsoul Fragment - King's": {
-            return "King Fragment";
+            return ["King Fragment", qualifier];
         }
         case "Relic Dealer Lemm":
         case "Relic Dealer Lemm Shop": {
-            return name.substr("Relic Dealer ".length);
+            return [name.substr("Relic Dealer ".length), qualifier];
         }
         case "Whispering Root": {
             // qualifier is the area
-            return `${qualifier} Root`;
+            return [`${qualifier} Root`, "Whispering Root"];
         }
         case "Zote Rescued Vengefly King": {
-            return "Vengefly King";
+            return ["Vengefly King", qualifier];
         }
         case "Mega Moss Charger": {
-            return "Massive Moss Charger";
+            return ["Massive Moss Charger", qualifier];
         }
         case "Nightmare Lantern Destroyed": {
-            return "Banishment";
+            return ["Banishment", qualifier];
         }
         default: {
             break;
         }
     }
 
-    return name;
+    return [name, qualifier];
 }
 
 export function parseSplitsDefinitions(): Map<string, SplitDefinition> {
@@ -154,12 +155,13 @@ export function parseSplitsDefinitions(): Map<string, SplitDefinition> {
             id,
             tooltip,
         } = match.groups;
-        const name = getName(description);
+        const [name, group] = getNameAndGroup(description);
         definitions.set(id, {
             description,
             id,
             tooltip,
             name,
+            group,
         });
     }
 
