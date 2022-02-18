2c4036d105a7edd3c6b6c35e58f55e74c59f3553
theeccentrician <theeccentrician@gmail.com>
2022-02-18 10:54:25 -0800

    hyperlink update proposal for instructions.tsx
    Update the hyperlink for the splits.txt file to point to https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/hollowknight/splits.txt.

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -9,7 +9,7 @@ const Instructions: React.FC = () => {
       <ol>
         <li>
                     Find the splits you want to use from the <a
-            href="https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/splits.txt"
+            href="https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/hollowknight/splits.txt"
             target="_blank" rel="noreferrer"
           >splits.txt</a> file. For example, if you want to split
                     on "Mask Fragment 4 (Upgrade)", then use "Mask1" as the
