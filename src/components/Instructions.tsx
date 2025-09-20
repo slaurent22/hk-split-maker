a0d4aa41e4a1567effbffd90eebac3ec4bc90d71
slaurent <slaurent22@pm.me>
2025-09-20 06:43:11 -0400

    update instructions

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { useCurrentGame } from "../hooks";
+import { Game } from "../store/game-slice";
 
 const VIDEO_URL = "https://www.youtube.com/watch?v=JBdm7LvZAZQ";
 
+const SPLITS_LINK: Record<Game, { href: string; text: string }> = {
+  hollowknight: {
+    href: "https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/hollowknight/splits.txt",
+    text: "splits.txt",
+  },
+  silksong: {
+    href: "https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/silksong/splits.json",
+    text: "splits.json",
+  },
+};
+
+const SPLITS_EXAMPLE: Record<Game, { name: string; id: string }> = {
+  hollowknight: {
+    name: "Mask Fragment 4 (Upgrade)",
+    id: "Mask1",
+  },
+  silksong: {
+    name: "Bell Beast (Boss)",
+    id: "BellBeast",
+  },
+};
+
 const Instructions: React.FC = () => {
+  const game = useCurrentGame();
   return (
     <div id="instructions">
-      <h2>Instructions</h2>
+      {game === "hollowknight" && <h2>Hollow Knight Instructions</h2>}
+      {game === "silksong" && <h2>Silksong Instructions</h2>}
       <p>
         To use a built-in configuration, select a category from the "Pre-made
         Category" box. You can scroll or search within it. Click "Generate",
@@ -15,23 +41,33 @@ const Instructions: React.FC = () => {
       <ul>
         <li>
           Find the splits you want to use from the{" "}
-          <a
-            href="https://github.com/slaurent22/hk-split-maker/blob/main/src/asset/hollowknight/splits.txt"
-            target="_blank"
-            rel="noreferrer"
-          >
-            splits.txt
+          <a href={SPLITS_LINK[game].href} target="_blank" rel="noreferrer">
+            {SPLITS_LINK[game].text}
           </a>{" "}
-          file. For example, if you want to split on "Mask Fragment 4
-          (Upgrade)", then use "Mask1" as the split name. As an easier
-          alternative, use the "Add autosplit" select box. You can type to
-          search or scroll within it. When you select an option, it will be
-          added to the <strong>bottom</strong> of the "splitIds".
+          file. For example, if you want to split on "
+          {SPLITS_EXAMPLE[game].name}" , then use "{SPLITS_EXAMPLE[game].id}" as
+          the split id. As an easier alternative, use the "Add autosplit" select
+          box. You can type to search or scroll within it. When you select an
+          option, it will be added to the <strong>bottom</strong> of the
+          "splitIds".
         </li>
         <li>
           List your desired splits in the "splitIds" section of the
           configuration.
           <ul>
+            {game === "hollowknight" && (
+              <li>
+                Timer will start automatically. The first splitId is the first
+                segment. For a different start, specify a
+                startTriggeringAutosplit.
+              </li>
+            )}
+            {game === "silksong" && (
+              <li>
+                The first entry is the start trigger. It is usually
+                "StartNewGame". It will not correspond to a Segment.
+              </li>
+            )}
             <li>
               Mark a split as a <strong>subsplit</strong> by prefixing the name
               with a minus sign (-). These entries will get an autosplit and a
